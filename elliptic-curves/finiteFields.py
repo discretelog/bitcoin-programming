26b530a1f7d0c4e628901bead04e40d1686f8930
Discrete Log <56081779+discretelog@users.noreply.github.com>
2021-12-18 18:54:58 -0800

    add test cases

--- elliptic-curves/finiteFields.py
+++ elliptic-curves/finiteFields.py
@@ -1,3 +1,4 @@
+import unittest
 
 class FieldElement:
 
@@ -56,3 +57,61 @@ class FieldElement:
             raise TypeError('Cannot divide numbers from different fields')
         result = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
         return self.__class__(result, self.prime)
+
+
+class FieldElementTest(unittest.TestCase):
+
+    def test_eq(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(2, 31)
+        self.assertTrue(a == b)
+
+    def test_ne(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(2, 31)
+        c = FieldElement(15, 31)
+        self.assertEqual(a, b)
+        self.assertTrue(a != c)
+        self.assertFalse(a != b)
+
+    def test_add(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(15, 31)
+        c = FieldElement(17, 31)
+        self.assertEqual(a + b, c)
+        a = FieldElement(17, 31)
+        b = FieldElement(21, 31)
+        self.assertEqual(a + b, FieldElement(7, 31))
+
+    def test_sub(self):
+        a = FieldElement(29, 31)
+        b = FieldElement(4, 31)
+        self.assertEqual(a - b, FieldElement(25, 31))
+        a = FieldElement(15, 31)
+        b = FieldElement(30, 31)
+        self.assertEqual(a - b, FieldElement(16, 31))
+
+    def test_mul(self):
+        a = FieldElement(24, 31)
+        b = FieldElement(19, 31)
+        self.assertEqual(a * b, FieldElement(22, 31))
+
+    def test_pow(self):
+        a = FieldElement(17, 31)
+        self.assertEqual(a**3, FieldElement(15, 31))
+        a = FieldElement(5, 31)
+        b = FieldElement(18, 31)
+        self.assertEqual(a**5 * b, FieldElement(16, 31))
+
+    def test_truediv(self):
+        a = FieldElement(3, 31)
+        b = FieldElement(24, 31)
+        self.assertEqual(a / b, FieldElement(4, 31))
+        a = FieldElement(17, 31)
+        self.assertEqual(a**-3, FieldElement(29, 31))
+        a = FieldElement(4, 31)
+        b = FieldElement(11, 31)
+        self.assertEqual(a**-4 * b, FieldElement(13, 31))
+
+if __name__ == '__main__':
+    unittest.main()
