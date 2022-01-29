7cb96133fe4aa649e744dc2de49cc9fabd661685
Discrete Log <56081779+discretelog@users.noreply.github.com>
2022-01-28 16:54:20 -0800

    weird changes

--- elliptic_curves/secp256k1.py
+++ elliptic_curves/secp256k1.py
@@ -54,6 +54,34 @@ class S256Point(Point):
             return b'\0x04' + self.x.num.to_bytes(32,'big') \
                    + self.y.num.to_bytes(32, 'big')
 
+    # sqrt function to get y from x. Uses Fermats little theorem
+    def sqrt(self):
+        return self ** ((P + 1) // 4)
+
+
+    # Parse public key
+    @classmethod
+    def parse(self, sec_bin):
+        if sec_bin[0] == 4:
+            x = int.from_bytes(sec_bin[1:33], 'big')
+            y = int.from_bytes(sec_bin[33:65], 'big')
+            return S256Point(x=x, y=y)
+        is_even = sec_bin[0] == 2:
+        x = S256Field(int.from_bytes(sec_bin[1:], 'big'))
+        alpha = x**3 + S256Field(B)
+        beta = alpha.sqrt()
+        if beta.num % 2 == 0:
+            even_beta = beta
+            odd_beta = S256Field(P - beta.num)
+        else:
+            even_beta = S256Field(P - beta.num)
+            odd_beta = beta
+        if is_even:
+            return S256Point(x, even_beta)
+        else:
+            return S256Point(x, odd_beta)
+
+
 
 G = S256Point(
     0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
