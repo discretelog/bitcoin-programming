a50c8b5293ed93315a349ca60c7346defe6b2cce
Discrete Log <56081779+discretelog@users.noreply.github.com>
2022-02-02 22:22:37 -0800

    add transaction fetcher for previous output amts

--- transactions/transactions.py
+++ transactions/transactions.py
@@ -103,3 +103,35 @@ class TxOut:
         result = int_to_little_endian(self.amount, 8)
         result += self.script_pubkey.serialize()
         return result
+
+class Fetcher:
+
+    cache = {}
+
+    @classmethod
+    def get_url(cls, test_net=False):
+        if testnet:
+            return 'http://testnet.programmingbitcoin.com'
+        else:
+            return 'http://mainnet.programmingbitcoin.com'
+
+    def fetch(cls, tx_id, testnet=False, fresh=False):
+        if fresh or (tx_id not in cache):
+            url = '{}/tx/{}.hex'.format(cls.get_url(testnet), tx_id)
+            response = requests.get(url)
+            try:
+                raw = bytes.fromhex(response.text.strip())
+            except ValueError:
+                raise ValueError('unexpected response: {}'.format(response.text))
+            if raw[4] == 0:
+                raw = raw[:4] + raw[6:]
+                tx = Tx.parse(BytesIO(raw), testnet=testnet)
+                tx.locktime = little_endian_to_int(raw[-4:])
+            else:
+                tx = Tx.parse(BytesIO(raw), testnet=testnet)
+            if tx.id() != tx_id:  1
+                raise ValueError('not the same id: {} vs {}'.format(tx.id(),
+                                  tx_id))
+            cls.cache[tx_id] = tx
+        cls.cache[tx_id].testnet = testnet
+        return cls.cache[tx_id]
