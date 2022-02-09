00faef269d3b891b5e8e073b360b0a51567c54f5
Discrete Log <56081779+discretelog@users.noreply.github.com>
2022-02-08 17:10:22 -0800

    add script class

---
+++ transactions/script.py
@@ -0,0 +1,21 @@
+
+class Script:
+
+    def __init__(self, cmds=None):
+        if cmds is None:
+            self.cmds = []
+        else:
+            self.cmds = cmds
+
+    def __repr__(self):
+        result = []
+        for cmd in self.cmds:
+            if type(cmd) == int:
+                if OP_CODE_NAMES.get(cmd):
+                    name = OP_CODE_NAMES.get(cmd)
+                else:
+                    name = 'OP_[{}]'.format(cmd)
+                result.append(name)
+            else:
+                result.append(cmd.hex())
+        return ' '.join(result)
