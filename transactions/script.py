fdabc0dcc991c7f50eb969fbb22c309826e1c61a
Discrete Log <56081779+discretelog@users.noreply.github.com>
2022-02-10 19:12:31 -0800

    add script parsing

--- transactions/script.py
+++ transactions/script.py
@@ -1,3 +1,8 @@
+from helper import (
+    parse_varint,
+    little_endian_to_int,
+    int_to_little_endian
+    )
 
 class Script:
 
@@ -19,3 +24,31 @@ class Script:
             else:
                 result.append(cmd.hex())
         return ' '.join(result)
+
+    @classmethod
+    def parse(cls, s):
+        length = read_varint(s)
+        cmds = []
+        count = 0
+        while count < length:
+            current = s.read(1)
+            count += 1
+            current_byte = current[0]
+            if current_byte >= 1 and current_byte <= 75:
+                n = current_byte
+                cmds.append(s.read(n))
+                count += n
+            elif current_byte == 76:
+                data_length = little_endian_to_int(s.read(1))
+                cmds.append(s.read(data_length))
+                count += data_length + 1
+            elif current_byte == 77:
+                data_length = little_endian_to_int(s.read(2))
+                cmds.append(s.read(data_length))
+                count += data_length + 2
+            else:
+                op_code = current_byte
+                cmds.append(op_code)
+        if count != length:
+            raise SyntaxError('parsing script failed')
+        return cls(cmds)
