9434561cd21ff6ef911cac009cee66f7a8ba2fba
Discrete Log <56081779+discretelog@users.noreply.github.com>
2022-02-12 20:13:55 -0800

    implement op_checksig

--- transactions/op_codes.py
+++ transactions/op_codes.py
@@ -58,6 +58,23 @@ def op_0(stack):
     stack.append(encode_num(0))
     return True
 
+
+def op_checksig(stack, z):
+    if len(stack) < 2:
+        return False
+    sec_pubkey = stack.pop()
+    der_signature = stack.pop()[:-1]
+    try:
+        point = S256Point.parse(sec_pubkey)
+        sig = Signature.parse(der_signature)
+    except (ValueError, SyntaxError) as e:
+        return False
+    if point.verify(z, sig):
+        stack.append(encode_num(1))
+    else:
+        stack.append(encode_num(0))
+    return True
+
 OP_CODE_FUNCTIONS = {
     0: op_0,
     79: op_1negate,
